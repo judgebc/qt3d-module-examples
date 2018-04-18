86153c28173bee313e90ac0392efe6de930992aa
Scott Baldric <ScottMBaldric@gmail.com>
2018-04-18 17:31:09 -0500

    All the includes.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,29 @@
 
 #include <QtGui\QScreen>
 
+#include <Qt3DCore\QTransform>
+#include <Qt3DCore\QAspectEngine>
+
+#include <Qt3DRender\QMesh>
+#include <Qt3DRender\QTechnique>
+#include <Qt3DRender\QMaterial>
+#include <Qt3DRender\QEffect>
+#include <Qt3DRender\QTexture>
+#include <Qt3DRender\QRenderPass>
+#include <Qt3DRender\QSceneLoader>
+#include <Qt3DRender\QPointLight>
+#include <Qt3DRender\QRenderAspect>
+
+#include <Qt3DInput\QInputAspect>
+
+#include <Qt3DExtras\qtorusmesh>
+#include <Qt3DExtras\QForwardRenderer>
+#include <Qt3DExtras\Qt3DWindow>
+#include <Qt3DExtras\QFirstPersonCameraController>
+
 int main(int argc, char ** argv)
 {
+	QApplication(argc, argv);
+
+
 }
