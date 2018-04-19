f68f50909a9bc724a4378387b086b1d825c6e17b
Scott Baldric <scott.baldric@beast-code.com>
2018-04-19 07:27:38 -0500

    Updating generate for work computer.

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/us/bin/env sh
 
-cmake -H. -Bbuild -G"Visual Studio 15 2017 Win64" -DCMAKE_PREFIX_PATH=/c/Qt/5.10.1/msvc2017_64/lib/cmake
+cmake -H. -Bbuild -G"Visual Studio 15 2017 Win64" -DCMAKE_PREFIX_PATH=/c/QtScott/5.10.1/msvc2017_64/lib/cmake
