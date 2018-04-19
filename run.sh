bcf73fe0fca0bdb9f4843892acf931a9c1555521
Scott Baldric <ScottMBaldric@gmail.com>
2018-04-19 18:17:45 -0500

    Expanding script to include --gen flag.

--- run.sh
+++ run.sh
@@ -16,7 +16,8 @@ then
 	print_folder_list
 
 	exit -1
-elif [[ ! -d $1 ]]; then
+elif [ ! -d $1 ]
+then
 	echo "Invalid directory. Choose from one of the following:"
 
 	print_folder_list
@@ -26,15 +27,23 @@ else
 	folder=$1
 fi
 
-if [[ -n "$2" ]]; then
-	config=$2
-else
-	config="Release"
-fi
-
 if [ ! -d $folder/build ]
 then
 	cmake -H$folder -B$folder/build -G"Visual Studio 15 2017 Win64" -DCMAKE_PREFIX_PATH=/c/Qt/5.10.1/msvc2017_64/lib/cmake
+fi
+
+only_gen="--gen"
+
+if [[ $2 = $only_gen ]]
+then
+	exit 0
+fi
+
+if [[ $2 = "Debug" || $2 = "Release" ]]
+then
+	config=$2
+else
+	config="Release"
 fi
 
 cmake --build $folder/build --config $config
