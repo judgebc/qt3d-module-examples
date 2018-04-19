eb44647ae599c5d2f9ff5dfc2cf8bfc365d16ecf
Scott Baldric <scott.baldric@beast-code.com>
2018-04-19 07:34:11 -0500

    Adding skeleton for project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <QGuiApplication>
 
+#include <Qt3DCore\qentity.h>
+#include <Qt3DCore\qaspectengine.h>
+
+#include <Qt3DRender\qcamera.h>
+#include <Qt3DRender\qrenderaspect.h>
+#include <Qt3DRender\qtextureimage.h>
+
+#include <Qt3DInput\qinputaspect.h>
+
+#include <Qt3DExtras\qphongalphamaterial.h>
+#include <Qt3DExtras\qdiffusemapmaterial.h>
+#include <Qt3DExtras\qforwardrenderer.h>
+#include <Qt3DExtras\qt3dwindow.h>
+#include <Qt3DExtras\qfirstpersoncameracontroller.h>
+
+Qt3DCore::QEntity * createSceneRoot();
+
 int main(int argc, char * argv[])
 {
   QGuiApplication app(argc, argv);
